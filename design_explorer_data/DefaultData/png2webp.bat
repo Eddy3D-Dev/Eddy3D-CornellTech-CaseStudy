dd74a25e579080abadb46166aa2a3a0787c03bf1
Patrick Kastner <patrick.kastner@gmail.com>
2022-08-10 00:48:20 -0400

    More Data

--- design_explorer_data/DefaultData/png2webp.bat
+++ design_explorer_data/DefaultData/png2webp.bat
@@ -1,4 +1,4 @@
 REM #Download dwebp (WebP decoder tool) https://developers.google.com/speed/webp/download
 REM #Run 
-for %%f in (*.png) do cwebp.exe -o "%%~nf.webp" "%%f"
+for %%f in (*.png) do cwebp.exe "%%f" -o "%%~nf.webp" -lossless
 PAUSE
